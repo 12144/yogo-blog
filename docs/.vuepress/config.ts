d2856113e5f5fba92cce4004a12aca5ed8728ee2
12144 <1214425528@qq.com>
2023-07-29 23:13:13 +0800

    init: 初始化博客

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,21 @@
+import {defineUserConfig, defaultTheme} from "vuepress"
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: '刘有果的博客',
+    theme: defaultTheme({
+        sidebar: {
+            '/': [
+                {
+                    text: '知识积累',
+                    children: [
+                        '/web/index.md',
+                        '/javascript/index.md',
+                        '/typescript/index.md',
+                        '/react/index.md'
+                    ]
+                }
+            ]
+        }
+    })
+})
