b0d73578a6ae1b266a789418df6ec0936934bf5d
王宇航 <wangyuhang.ollie@bytedance.com>
2023-09-25 00:30:52 +0800

    note: 前端调试方法的原理

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,15 +1,15 @@
 import { defineUserConfig, defaultTheme } from "vuepress";
-import {registerComponentsPlugin} from '@vuepress/plugin-register-components'
-import { getDirname, path } from '@vuepress/utils'
+import { registerComponentsPlugin } from "@vuepress/plugin-register-components";
+import { getDirname, path } from "@vuepress/utils";
 
-const __dirname = getDirname(import.meta.url)
+const __dirname = getDirname(import.meta.url);
 
 export default defineUserConfig({
   lang: "zh-CN",
   title: "刘有果的博客",
   plugins: [
     registerComponentsPlugin({
-      componentsDir: path.resolve(__dirname, './components'),
+      componentsDir: path.resolve(__dirname, "./components"),
     }),
   ],
   theme: defaultTheme({
@@ -18,33 +18,45 @@ export default defineUserConfig({
         {
           text: "知识积累",
           children: [
-            "/web/index.md",
+            {
+              text: "web",
+              children: [
+                {
+                  text: "调试",
+                  children: ["/web/调试/前端各种调试方法的原理"],
+                },
+              ],
+            },
             "/javascript/index.md",
             "/typescript/index.md",
             {
               text: "react",
-              children: ["/react/profiler", "/react/ref使用技巧","/react/react18可以提高性能的新feature" , 
-              {
-                text: "状态管理",
-                children: ["/react/状态管理/jotai"]
-              }],
+              children: [
+                "/react/profiler",
+                "/react/ref使用技巧",
+                "/react/react18可以提高性能的新feature",
+                {
+                  text: "状态管理",
+                  children: ["/react/状态管理/jotai"],
+                },
+              ],
             },
             {
               text: "pnpm",
-              children: ["/pnpm/管理pnpm的版本"]
+              children: ["/pnpm/管理pnpm的版本"],
             },
             {
               text: "vscode",
-              children: ["/vscode/vscode提效技巧"]
+              children: ["/vscode/vscode提效技巧"],
             },
             {
-              text: 'svg',
-              children: ["/svg/svg基础知识"]
+              text: "svg",
+              children: ["/svg/svg基础知识"],
             },
             {
-              text: '民生政策',
-              children: ['/民生政策/租房公积金提取']
-            }
+              text: "民生政策",
+              children: ["/民生政策/租房公积金提取"],
+            },
           ],
         },
       ],
